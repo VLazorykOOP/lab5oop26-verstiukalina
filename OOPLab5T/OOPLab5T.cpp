6253478354f0de95afa18266660abb95f501700a
IvanSavcynskyi <ivansavchyn2207@gmail.com>
2026-04-30 22:43:32 +0300

    Created Point, MyEllipse and Circle

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -1,24 +1,87 @@
-﻿// OOPLab5T.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <windows.h>
+#undef Ellipse
+using namespace std;
 
+class Point {
+protected:
+    double x, y;
 
-#include <iostream>
+public:
+    Point() {
+        x = 0;
+        y = 0;
+    }
 
-// Ваші файли загловки 
-//
-#include "Lab5Exmaple.h"
-int main()
-{
-    std::cout << " Lab #5  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
+    Point(double x1, double y1) {
+        x = x1;
+        y = y1;
+    }
 
-    int chain = 1;
-    if (chain == 1) chain = mainExample1();
-    if (chain == 2) chain = mainExample2();
-    if (chain == 3) chain = mainExample3();
-    if (chain == 4) chain = mainExample4();
-    if (chain == 5) chain = mainExample5();
+    ~Point() {
+        cout << "Деструктор точки\n";
+    }
 
-}
+    void drukuvaty() {
+        cout << "Точка: x = " << x << ", y = " << y << endl;
+    }
+
+    double ploshcha() {
+        return 0;
+    }
+};
+
+class MyEllipse : public Point {
+protected:
+    double a, b;
+
+public:
+    MyEllipse() : Point() {
+        a = 1;
+        b = 1;
+    }
+
+    MyEllipse(double x1, double y1, double a1, double b1) : Point(x1, y1) {
+        a = a1;
+        b = b1;
+    }
+
+    ~MyEllipse() {
+        cout << "Деструктор еліпса\n";
+    }
+
+    void drukuvaty() {
+        cout << "Центр еліпса: x = " << x << ", y = " << y << endl;
+        cout << "Півосі: a = " << a << ", b = " << b << endl;
+        cout << "Площа = " << ploshcha() << endl;
+    }
+
+    double ploshcha() {
+        return 3.14 * a * b;
+    }
+};
+
+class Circle : public MyEllipse {
+public:
+    Circle() : MyEllipse() {}
+
+    Circle(double x1, double y1, double r) : MyEllipse(x1, y1, r, r) {}
+
+    ~Circle() {
+        cout << "Деструктор кола\n";
+    }
+
+    void drukuvaty() {
+        cout << "Центр кола: x = " << x << ", y = " << y << endl;
+        cout << "Радіус: r = " << a << endl;
+        cout << "Площа = " << ploshcha() << endl;
+    }
+
+    double ploshcha() {
+        return 3.14 * a * a;
+    }
+};
+
